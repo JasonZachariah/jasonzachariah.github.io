35e9cea5400fcf9f42b4e949cc6e2de7c96f3cb0
JasonZachariah <jasontzachariah@gmail.com>
2025-04-06 19:17:40 -0400

    card animations

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+    theme: {
+       extend: {}, 
+   },
+  plugins: [require('tailwindcss-motion')], 
+};
+
+module.exports = {
+    // ...
+    plugins: [
+      require('tailwindcss-animated')
+    ],
+  }
