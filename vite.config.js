adbd3bb289b3566b5f96651c6b084c0ab102fba9
JasonZachariah <jasontzachariah@gmail.com>
2025-04-28 09:42:47 -0400

    vite chages

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname,'index.html' ) ,     //
-
-        //project pages
-        project1: resolve(__dirname,'octobud.html'),
-        project2: resolve(__dirname,'fitfam.html'),
-        project3:resolve(__dirname, 'handbook.html')
+        main: './index.html',
+        octobud: './octobud.html', 
       }
-    }
+    },
+    outDir: 'dist', 
   }
-})
+});
