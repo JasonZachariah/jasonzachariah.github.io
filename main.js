49588614c8bfc0e9f345808aafb9accc76d3e7bb
JasonZachariah <jasontzachariah@gmail.com>
2025-04-28 17:07:33 -0400

    head aniamtion change

--- main.js
+++ main.js
@@ -4,16 +4,25 @@ animate(svg.createDrawable('.line'), {
   draw: ['0.5 2'],
   ease: 'inOutQuad',
   
-    
+  
   duration: 2000,
   
   delay: stagger(100),
 
 });
 
+animate('.line',{
+  delay:1500,
+  fill: ['#89f336', '#000'], 
+    duration: 200,
+
+
+    easing: 'outElastic'
+})
+
 animate('#logo', {
    delay:1500,
-    scale: [ 1.2, 1],
+    scale: [ 1.1, 1],
   
     duration: 1000,
     
