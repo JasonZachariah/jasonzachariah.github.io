c082f04e0ba796e78fe5e494e2ff805862bb2e72
JasonZachariah <jasontzachariah@gmail.com>
2025-04-27 21:38:09 -0400

    vit changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',       //
+        main: resolve(__dirname,'index.html' ) ,     //
 
         //project pages
-        project1: 'octobud.html',
-        project2: 'fitfam.html',
-        project3: 'handbook.html'
+        project1: resolve(__dirname,'octobud.html'),
+        project2: resolve(__dirname,'fitfam.html'),
+        project3:resolve(__dirname, 'handbook.html')
       }
     }
   }
