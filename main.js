36b15c58edad981f0526cc781338f63475f80e84
JasonZachariah <jasontzachariah@gmail.com>
2025-04-28 15:39:55 -0400

    head animation change

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 import { animate, svg, stagger } from 'animejs';
 
 animate(svg.createDrawable('.line'), {
-  draw: ['0 1'],
+  draw: ['0.5 2'],
   ease: 'inOutQuad',
-  duration: 2000,
-  delay: stagger(100),
   
+    
+  duration: 2000,
+  
+  delay: stagger(100),
+
 });
+
+animate('#logo', {
+   delay:1500,
+    scale: [ 1.2, 1],
+  
+    duration: 1000,
+    
+    ease: 'outElastic', // ease applied between each keyframes if no ease defined
+ 
+   
+  });
