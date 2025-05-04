64ad7f8d9eb5384bac4b823522594162d2e87c63
JasonZachariah <jasontzachariah@gmail.com>
2025-05-04 18:01:03 -0400

    hero animation gsap added

--- main.js
+++ main.js
@@ -1,66 +1,47 @@
-import { animate, svg, stagger } from 'animejs';
+import { animate, svg, stagger } from "animejs";
 import { gsap } from "gsap";
-    
+
 import { SplitText } from "gsap/SplitText";
 
-gsap.registerPlugin(SplitText);
 
-
-animate(svg.createDrawable('.line'), {
-  draw: ['0.5 2'],
-  ease: 'inOutQuad',
-  
-  
-  duration: 2000,
-  
-  delay: stagger(100),
-
+let split = SplitText.create(".text", { 
+  type: "words, chars",
+  mask:"chars"
 });
 
-animate('.line',{
-  delay:1500,
-  fill: ['#FF', '#000'], 
-    duration: 200,
+// now animate the characters in a staggered fashion
+gsap.from(split.chars, {
+  duration: 1, 
+
+  y: 100,       // animate from 100px below
+  autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
+  stagger: 0.1 // 0.05 seconds between each
+});
 
 
-    easing: 'outElastic'
-})
+animate(svg.createDrawable(".line"), {
+  draw: ["0.5 2"],
+  ease: "inOutQuad",
 
-animate('#logo', {
-   delay:1500,
-    scale: [ 1.1, 1],
-  
-    duration: 1000,
-    
-    ease: 'outElastic', // ease applied between each keyframes if no ease defined
- 
-   
-  });
+  duration: 2000,
 
+  delay: stagger(100),
+});
 
+animate(".line", {
+  delay: 1500,
+  fill: ["#FF", "#000"],
+  duration: 200,
 
-  let split;
-  SplitText.create(".line", {
-    type: "char,lines",
-    linesClass: "line",
-    autoSplit: true,
-    mask: "lines",
-    onSplit: (self) => {
-      split = gsap.from(self.lines, {
-        duration: 2,
-        yPercent: 100,
-        opacity: 0,
-        stagger: 0.7,
-        ease: "expo.out",
-      });
-      return split;
-    }
-  });
+  easing: "outElastic",
+});
 
-function setup() {
-  split && split.revert();
-  animation && animation.revert();
-  split = SplitText.create(".text", {type:"chars"});
-}
-setup();
-window.addEventListener("resize", setup);
+animate("#logo", {
+  delay: 1500,
+  scale: [1.1, 1],
+
+  duration: 1000,
+
+  ease: "outElastic", // ease applied between each keyframes if no ease defined
+});
+
