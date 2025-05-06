7dec3b70c7d377b1d2fb786630185d1185c7b25b
JasonZachariah <jasontzachariah@gmail.com>
2025-05-05 21:22:20 -0400

    delete anime js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-import { animate, svg, stagger } from "animejs";
+
 import { gsap } from "gsap";
 
 import { SplitText } from "gsap/SplitText";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
+gsap.registerPlugin(DrawSVGPlugin) ;
 
 let split = SplitText.create(".text", { 
   type: "words, chars",
@@ -11,37 +13,14 @@ let split = SplitText.create(".text", {
 
 // now animate the characters in a staggered fashion
 gsap.from(split.chars, {
-  duration: 1, 
 
-  y: 100,       // animate from 100px below
+  ease: "expo.out",
+  y: 500,       // animate from 100px below
   autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
   stagger: 0.1 // 0.05 seconds between each
 });
 
 
-animate(svg.createDrawable(".line"), {
-  draw: ["0.5 2"],
-  ease: "inOutQuad",
-
-  duration: 2000,
-
-  delay: stagger(100),
-});
-
-animate(".line", {
-  delay: 1500,
-  fill: ["#FF", "#000"],
-  duration: 200,
-
-  easing: "outElastic",
-});
-
-animate("#logo", {
-  delay: 1500,
-  scale: [1.1, 1],
-
-  duration: 1000,
-
-  ease: "outElastic", // ease applied between each keyframes if no ease defined
-});
+//draws all elements with the "draw-me" class applied with staggered start times 0.1 seconds apart
+gsap.from(".line", {duration:1,stagger: 0.1, drawSVG: 0});
 
