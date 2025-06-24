7245ba683c88ec42a4a0f811c5c6202d531cc30e
JasonZachariah <jasontzachariah@gmail.com>
2025-06-24 13:43:13 -0400

    updated style

--- main.js
+++ main.js
@@ -30,24 +30,3 @@ function initPageScripts() {
   // re-initialize sliders, listeners, animations, etc.
 }
 
-import barba from '@barba/core';
-
-barba.init({
-  
-    transitions: [{
-        name: 'opacity-transition',
-        leave(data) {
-          return gsap.to(data.current.container, {
-            opacity: 0
-          });
-        },
-        enter(data) {
-          return gsap.from(data.next.container, {
-            opacity: 0
-          });
-        }
-      }]
-      
-  });
-
-  
