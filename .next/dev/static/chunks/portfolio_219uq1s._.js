8c73e8d9229fef78f6c5499df90f7b0d652f1ddb
JasonZachariah <jasontzachariah@gmail.com>
2026-08-20 19:01:01 -0400

    clear cache

--- .next/dev/static/chunks/portfolio_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1bza5tr._.css","static/chunks/05ot_next_dist_12cczy2._.js"],
-    source: "entry"
-});
--- .next/dev/static/chunks/portfolio_pages__error_0du2_q-._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/05ot_next_dist_compiled_0vgnixv._.js","static/chunks/05ot_next_dist_shared_lib_0azcou-._.js","static/chunks/05ot_next_dist_client_0x1u8ba._.js","static/chunks/05ot_next_dist_11cbej4._.js","static/chunks/[next]_entry_page-loader_ts_0v33avs._.js","static/chunks/05ot_react-dom_1l0x9hj._.js","static/chunks/05ot_1ogqkae._.js","static/chunks/[root-of-the-server]__201dfot._.js"],
-    source: "entry"
-});
