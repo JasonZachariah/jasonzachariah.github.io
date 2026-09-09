e5b8ded704b3078226bceb4cb8d26ebea71c9cf0
JasonZachariah <jasontzachariah@gmail.com>
2026-09-09 13:09:36 -0400

    optimize fonts

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,7 +1,37 @@
 import type { Metadata } from "next";
+import { Overpass } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
+const overpass = Overpass({
+  subsets: ["latin"],
+  variable: "--font-overpass",
+  display: "swap",
+});
+
+const ppSansRounded = localFont({
+  src: [
+    {
+      path: "../public/fonts/PPPangramSansRounded-NarrowSemibold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/PPPangramSansRounded-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/PPPangramSansRounded-Bold.otf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-pp-sans-rounded",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Jason Zachariah",
@@ -18,8 +48,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={`${overpass.variable} ${ppSansRounded.variable}`}>
+      <body className={overpass.className} suppressHydrationWarning>
         <Header />
         {children}
         <Footer />
